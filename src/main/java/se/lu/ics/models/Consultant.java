795f6aab3b887ee2052adb793a5b068bc5ebe547
Sophia Udén <sophia.uden@gmail.com>
2024-09-20 12:53:13 +0200

    Populate model classes

--- src/main/java/se/lu/ics/models/Consultant.java
+++ src/main/java/se/lu/ics/models/Consultant.java
@@ -1,5 +1,59 @@
 package se.lu.ics.models;
 
-public class Consultant {
+import java.time.LocalDate;
 
+public class Consultant {
+    private String EmpNo;
+    private String EmpFirstName;
+    private String EmpLastName;
+    private String EmpTitle;
+    private LocalDate EmpStartDate;
+
+    public Consultant(String EmpNo, String EmpFirstName, String EmpLastName, String EmpTitle, LocalDate StartDate) {
+        this.EmpNo = EmpNo;
+        this.EmpFirstName = EmpFirstName;
+        this.EmpLastName = EmpLastName;
+        this.EmpTitle = EmpTitle;
+        this.EmpStartDate = StartDate;
+    }
+
+    public String getEmpNo() {
+        return EmpNo;
+    }
+
+    public void setEmpNo(String EmpNo) {
+        this.EmpNo = EmpNo;
+    }
+
+    public String getEmpFirstName() {
+        return EmpFirstName;
+    }
+
+    public void setEmpFirstName(String EmpFirstName) {
+        this.EmpFirstName = EmpFirstName;
+    }
+
+    public String getEmpLastName() {
+        return EmpLastName;
+    }
+
+    public void setEmpLastName(String EmpLastName) {
+        this.EmpLastName = EmpLastName;
+    }
+
+    public String getEmpTitle() {
+        return EmpTitle;
+    }
+
+    public void setEmpTitle(String EmpTitle) {
+        this.EmpTitle = EmpTitle;
+    }
+
+    public LocalDate getEmpStartDate() {
+        return EmpStartDate;
+    }
+
+    public void setEmpStartDate(LocalDate StartDate) {
+        this.EmpStartDate = StartDate;
+    }
 }
--- src/main/java/se/lu/ics/models/Milestone.java
+++ src/main/java/se/lu/ics/models/Milestone.java
@@ -1,5 +1,39 @@
 package se.lu.ics.models;
 
-public class Milestone {
+import java.time.LocalDate;
 
+public class Milestone {
+    private String MilestoneNo;
+    private String MilestoneName;
+    private LocalDate MilestoneDate;
+
+    public Milestone(String MilestoneNo, String MilestoneName) {
+        this.MilestoneNo = MilestoneNo;
+        this.MilestoneName = MilestoneName;
+        this.MilestoneDate = LocalDate.now();
+    }
+
+    public String getMilestoneNo() {
+        return MilestoneNo;
+    }
+
+    public void setMilestoneNo(String MilestoneNo) {
+        this.MilestoneNo = MilestoneNo;
+    }
+
+    public String getMilestoneName() {
+        return MilestoneName;
+    }
+
+    public void setMilestoneName(String MilestoneName) {
+        this.MilestoneName = MilestoneName;
+    }
+    
+    public LocalDate getMilestoneDate() {
+        return MilestoneDate;
+    }
+
+    public void setMilestoneDate(LocalDate MilestoneDate) {
+        this.MilestoneDate = MilestoneDate;
+    }
 }
