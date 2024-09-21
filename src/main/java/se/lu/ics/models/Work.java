5639bb4273cd47d5166982051d841a490a3fc1a8
Emelie <emelievons@gmail.com>
2024-09-21 13:50:07 +0200

    Create model class Work

---
+++ src/main/java/se/lu/ics/models/Work.java
@@ -0,0 +1,38 @@
+package se.lu.ics.models;
+
+public class Work {
+
+    private String projectNo;
+    private String consultantNo;
+    private double workHours;
+
+    public Work(String projectNo, String consultantNo, double workHours) {
+        this.projectNo = projectNo;
+        this.consultantNo = consultantNo;
+        this.workHours = workHours;
+    }
+
+    public String getProjectNo() {
+        return projectNo;
+    }
+
+    public void setProjectNo(String projectNo) {
+        this.projectNo = projectNo;
+    }
+
+    public String getConsultantNo() {
+        return consultantNo;
+    }
+
+    public void setConsultantNo(String consultantNo) {
+        this.consultantNo = consultantNo;
+    }
+
+    public double getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(double workHours) {
+        this.workHours = workHours;
+    }
+}
