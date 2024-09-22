d5b48c12e5e576d13e8e04417cddf7c227f6b73e
Emelie <emelievons@gmail.com>
2024-09-22 12:10:40 +0200

    Add constructor in primary controller

--- src/main/java/se/lu/ics/controllers/PrimaryController.java
+++ src/main/java/se/lu/ics/controllers/PrimaryController.java
@@ -3,6 +3,7 @@ package se.lu.ics.controllers;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import se.lu.ics.App;
+import se.lu.ics.data.ConsultantDao;
 
 public class PrimaryController {
 
@@ -10,4 +11,15 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    private ConsultantDao consultantDao;
+
+    public PrimaryController() throws Exception { // Maybe doesnt need to throw exception, instead display error message
+        try {
+            consultantDao = new ConsultantDao();
+        } catch (IOException e) {
+            throw new Exception("Error initializing database connection: " + e.getMessage());
+        }
+    }
+
 }
