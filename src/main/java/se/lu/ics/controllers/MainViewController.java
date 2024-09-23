e7728eced0e71d649b8eb3bc5331b55694fae847
Sophia Udén <sophia.uden@gmail.com>
2024-09-23 15:10:55 +0200

    fixat register och remove consultantknappar

--- src/main/java/se/lu/ics/controllers/MainViewController.java
+++ src/main/java/se/lu/ics/controllers/MainViewController.java
@@ -1,5 +1,35 @@
 package se.lu.ics.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+
 public class MainViewController {
 
+    // Register Consultant Pane
+    @FXML 
+    private AnchorPane registerConsultantPane;
+
+    @FXML
+    private Button showRegisterConsultantPane;
+
+    @FXML
+    private void handleShowRegisterConsultantPane() {
+        registerConsultantPane.setVisible(true);
+        removeConsultantPane.setVisible(false);
+    }
+
+    //Remove consultant Pane
+    @FXML
+    private AnchorPane removeConsultantPane;
+
+    @FXML
+    private Button showRemoveConsultantPane;
+
+    @FXML
+    private void handleShowRemoveConsultantPane() {
+        removeConsultantPane.setVisible(true);
+        registerConsultantPane.setVisible(false);
+    }
 }
