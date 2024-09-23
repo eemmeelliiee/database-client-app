a543fffc7132e4351b07079a2b2089f43b0b6486
peggysch <pe8260sc-s@student.lu.se>
2024-09-23 17:11:56 +0200

    Add functionality in MainViewController

--- src/main/java/se/lu/ics/controllers/MainViewController.java
+++ src/main/java/se/lu/ics/controllers/MainViewController.java
@@ -69,5 +69,213 @@ public class MainViewController {
     }
 
 
+    //Project Tab 
 
+    //Register Project Pane
+    @FXML 
+    private AnchorPane registerProjectPane;
+
+    @FXML
+    private Button showRegisterProjectPane;
+
+    @FXML
+    private void handleShowRegisterProjectPane() {
+        registerProjectPane.setVisible(true);
+        removeProjectPane.setVisible(false);
+        updateProjectPane.setVisible(false);
+        infoProjectPane.setVisible(false);
+    }
+
+    //Remove Project Pane
+    @FXML
+    private AnchorPane removeProjectPane;
+
+    @FXML
+    private Button showRemoveProjectPane;
+
+    @FXML
+    private void handleShowRemoveProjectPane() {
+        removeProjectPane.setVisible(true);
+        registerProjectPane.setVisible(false);
+        updateProjectPane.setVisible(false);
+        infoProjectPane.setVisible(false);
+    }
+
+    // Update Project Pane
+    @FXML
+    private AnchorPane updateProjectPane;
+
+    @FXML
+    private Button showUpdateProjectPane;
+
+    @FXML
+    private void handleShowUpdateProjectPane() {
+        updateProjectPane.setVisible(true);
+        registerProjectPane.setVisible(false);
+        removeProjectPane.setVisible(false);
+        infoProjectPane.setVisible(false);
+    
+    }
+
+    // info pane
+    @FXML
+    private AnchorPane infoProjectPane;
+
+    @FXML
+    private Button showInfoProjectPane;
+
+    @FXML
+    private void handleShowInfoProjectPane() {
+        infoProjectPane.setVisible(true);
+        registerProjectPane.setVisible(false);
+        removeProjectPane.setVisible(false);
+        updateProjectPane.setVisible(false);
+    }
+
+    //Milestone Tab
+
+    //Register Milestone Pane
+    @FXML
+    private AnchorPane registerMSPane;
+
+    @FXML
+    private Button showRegisterMSPane;
+
+    @FXML
+    private void handleShowRegisterMSPane() {
+        registerMSPane.setVisible(true);
+        removeMSPane.setVisible(false);
+        addMSPane.setVisible(false);
+        retrieveMSPane.setVisible(false);
+    }
+
+    //Remove Milestone Pane
+    @FXML
+    private AnchorPane removeMSPane;
+
+    @FXML
+    private Button showRemoveMSPane;
+
+    @FXML
+    private void handleShowRemoveMSPane() {
+        removeMSPane.setVisible(true);
+        registerMSPane.setVisible(false);
+        addMSPane.setVisible(false);
+        retrieveMSPane.setVisible(false);
+    }
+
+    // Update Milestone Pane
+    @FXML
+    private AnchorPane addMSPane;
+
+    @FXML
+    private Button showAddMSPane;
+
+    @FXML
+    private void handleShowAddMSPane() {
+        addMSPane.setVisible(true);
+        registerMSPane.setVisible(false);
+        removeMSPane.setVisible(false);
+        retrieveMSPane.setVisible(false);
+    
+    }
+
+    // retrieve pane
+    @FXML
+    private AnchorPane retrieveMSPane;
+
+    @FXML
+    private Button showRetrieveMSPane;
+
+    @FXML
+    private void handleShowRetrieveMSPane() {
+        retrieveMSPane.setVisible(true);
+        registerMSPane.setVisible(false);
+        removeMSPane.setVisible(false);
+        addMSPane.setVisible(false);
+    }
+
+    //Work Tab
+
+    //Add Consultant to Project Pane
+    @FXML
+    private AnchorPane addConToProPane;
+
+    @FXML
+    private Button showAddConToProPane;
+
+    @FXML
+    private void handleShowAddConToProPane() {
+        addConToProPane.setVisible(true);
+        checkWorkingConsPane.setVisible(false);
+        updateHoursPane.setVisible(false);
+        displayConsHoursPane.setVisible(false);
+        hardestWorkingConPane.setVisible(false);
+    }
+
+    //Check Working Consultants Pane
+    @FXML
+    private AnchorPane checkWorkingConsPane;
+
+    @FXML
+    private Button showCheckWorkingConsPane;
+
+    @FXML
+    private void handleShowCheckWorkingConsPane() {
+        checkWorkingConsPane.setVisible(true);
+        addConToProPane.setVisible(false);
+        updateHoursPane.setVisible(false);
+        displayConsHoursPane.setVisible(false);
+        hardestWorkingConPane.setVisible(false);
+    }
+
+    // Update Hours Pane
+    @FXML
+    private AnchorPane updateHoursPane;
+
+    @FXML
+    private Button showUpdateHoursPane;
+
+    @FXML
+    private void handleShowUpdateHoursPane() {
+        updateHoursPane.setVisible(true);
+        addConToProPane.setVisible(false);
+        checkWorkingConsPane.setVisible(false);
+        displayConsHoursPane.setVisible(false);
+        hardestWorkingConPane.setVisible(false);
+    }
+
+    // Display Consultant Hours Pane
+    @FXML
+    private AnchorPane displayConsHoursPane;
+
+    @FXML
+    private Button showDisplayConsHoursPane;
+
+    @FXML
+    private void handleShowDisplayConsHoursPane() {
+        displayConsHoursPane.setVisible(true);
+        addConToProPane.setVisible(false);
+        checkWorkingConsPane.setVisible(false);
+        updateHoursPane.setVisible(false);
+        hardestWorkingConPane.setVisible(false);
+    }
+
+    // Hardest Working Consultant Pane
+    @FXML
+    private AnchorPane hardestWorkingConPane;
+
+    @FXML
+    private Button showHardestWorkingConPane;
+
+    @FXML
+    private void handleShowHardestWorkingConPane() {
+        hardestWorkingConPane.setVisible(true);
+        addConToProPane.setVisible(false);
+        checkWorkingConsPane.setVisible(false);
+        updateHoursPane.setVisible(false);
+        displayConsHoursPane.setVisible(false);
+    }
+
+    //MetaData Tab
 }
