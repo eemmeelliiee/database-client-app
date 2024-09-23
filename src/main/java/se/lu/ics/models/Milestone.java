21ea3457ac56738bfe520263e206aca89028953b
Emelie <emelievons@gmail.com>
2024-09-23 11:51:45 +0200

    Add javadoc comments for constructors

--- src/main/java/se/lu/ics/models/Milestone.java
+++ src/main/java/se/lu/ics/models/Milestone.java
@@ -8,9 +8,30 @@ public class Milestone {
     private String milestoneName;
     private LocalDate milestoneDate;
 
+    /**
+     * Main constructor for initializing Milestone objects. 
+     *
+     * @param milestoneNo The milestone number.
+     * @param projectNo The project number.
+     * @param milestoneName The milestone name.
+     * @param milestoneDate The milestone date.
+     */
     public Milestone(String milestoneNo, String projectNo, String milestoneName, LocalDate milestoneDate) {
         this.milestoneNo = milestoneNo;
         this.projectNo = projectNo;
+        this.milestoneName = milestoneName;
+        this.milestoneDate = milestoneDate;
+    }
+
+    /**
+     * Used in mapToMilestone() in MilestoneDao to retrieve all Milestones for Project.
+     *
+     * @param milestoneNo   The milestone number.
+     * @param milestoneName The milestone name.
+     * @param milestoneDate The milestone date.
+     */
+    public Milestone(String milestoneNo, String milestoneName, LocalDate milestoneDate) {
+        this.milestoneNo = milestoneNo;
         this.milestoneName = milestoneName;
         this.milestoneDate = milestoneDate;
     }
@@ -23,11 +44,11 @@ public class Milestone {
         this.milestoneNo = milestoneNo;
     }
 
-    public String getProjectNo(){
+    public String getProjectNo() {
         return projectNo;
     }
 
-    public void setProjectNo(String projectNo){
+    public void setProjectNo(String projectNo) {
         this.projectNo = projectNo;
     }
 
@@ -38,12 +59,22 @@ public class Milestone {
     public void setMilestoneName(String milestoneName) {
         this.milestoneName = milestoneName;
     }
-    
+
     public LocalDate getMilestoneDate() {
         return milestoneDate;
     }
 
     public void setMilestoneDate(LocalDate milestoneDate) {
         this.milestoneDate = milestoneDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Milestone{" +
+                "milestoneNo='" + milestoneNo + '\'' +
+                ", projectNo='" + projectNo + '\'' +
+                ", milestoneName='" + milestoneName + '\'' +
+                ", milestoneDate=" + milestoneDate +
+                '}';
     }
 }
--- src/main/java/se/lu/ics/models/Project.java
+++ src/main/java/se/lu/ics/models/Project.java
@@ -8,8 +8,18 @@ public class Project {
     private LocalDate projectStartDate;
     private LocalDate projectEndDate;
     private String projectStatus; // ProjectStatus is a computed column in SQL Server
-    
-    // projectStatus is thus set through setProjectStatus() in ProjectDao 
+
+    /**
+     * Main constructor for initializing Project objects.
+     * Since ProjectStatus is a computed column, it is set through setProjectStatus
+     * in ProjectDao(), and not in constructor.
+     * Constructs a Project object with the specified details.
+     * 
+     * @param projectNo        The project number.
+     * @param projectName      The project name.
+     * @param projectStartDate The start date of the project.
+     * @param projectEndDate   The end date of the project.
+     */
     public Project(String projectNo, String projectName, LocalDate projectStartDate, LocalDate projectEndDate) {
         this.projectNo = projectNo;
         this.projectName = projectName;
@@ -17,8 +27,16 @@ public class Project {
         this.projectEndDate = projectEndDate;
     }
 
-    // Used in mapToProject() in ProjectDao to retrieve all values for project instance
-    public Project(String projectNo, String projectName, LocalDate projectStartDate, LocalDate projectEndDate, String projectStatus) {
+    /**
+     * Used in mapToProject() in MilestoneDao to retrieve all values for project
+     * instance.
+     *
+     * @param milestoneNo   The milestone number.
+     * @param milestoneName The milestone name.
+     * @param milestoneDate The milestone date.
+     */
+    public Project(String projectNo, String projectName, LocalDate projectStartDate, LocalDate projectEndDate,
+            String projectStatus) {
         this.projectNo = projectNo;
         this.projectName = projectName;
         this.projectStartDate = projectStartDate;
@@ -50,19 +68,19 @@ public class Project {
         this.projectStartDate = projectStartDate;
     }
 
-    public LocalDate getProjectEndDate(){
+    public LocalDate getProjectEndDate() {
         return projectEndDate;
     }
 
-    public void setProjectEndDate(LocalDate projectEndDate){
+    public void setProjectEndDate(LocalDate projectEndDate) {
         this.projectEndDate = projectEndDate;
     }
-   
-    public String getProjectStatus(){
+
+    public String getProjectStatus() {
         return projectStatus;
     }
 
-    public void setProjectStatus(String projectStatus){
+    public void setProjectStatus(String projectStatus) {
         this.projectStatus = projectStatus;
     }
 
@@ -78,4 +96,3 @@ public class Project {
                 '}';
     }
 }
-
