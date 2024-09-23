97208d6689de72f1ab55ef280f68332a3ca537c2
Sophia Udén <sophia.uden@gmail.com>
2024-09-23 16:09:45 +0200

    fixat update och info till consultant tab

--- src/main/java/se/lu/ics/controllers/MainViewController.java
+++ src/main/java/se/lu/ics/controllers/MainViewController.java
@@ -18,6 +18,8 @@ public class MainViewController {
     private void handleShowRegisterConsultantPane() {
         registerConsultantPane.setVisible(true);
         removeConsultantPane.setVisible(false);
+        updateConsultantPane.setVisible(false);
+        infoPane.setVisible(false);
     }
 
     //Remove consultant Pane
@@ -31,5 +33,41 @@ public class MainViewController {
     private void handleShowRemoveConsultantPane() {
         removeConsultantPane.setVisible(true);
         registerConsultantPane.setVisible(false);
+        updateConsultantPane.setVisible(false);
+        infoPane.setVisible(false);
     }
+
+    // Update Consultant Pane
+    @FXML
+    private AnchorPane updateConsultantPane;
+
+    @FXML
+    private Button showUpdateConsultantPane;
+
+    @FXML
+    private void handleShowUpdateConsultantPane() {
+        updateConsultantPane.setVisible(true);
+        registerConsultantPane.setVisible(false);
+        removeConsultantPane.setVisible(false);
+        infoPane.setVisible(false);
+    
+    }
+
+    // info pane
+    @FXML
+    private AnchorPane infoPane;
+
+    @FXML
+    private Button showInfoPane;
+
+    @FXML
+    private void handleShowInfoPane() {
+        infoPane.setVisible(true);
+        registerConsultantPane.setVisible(false);
+        removeConsultantPane.setVisible(false);
+        updateConsultantPane.setVisible(false);
+    }
+
+
+
 }
